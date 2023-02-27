412cde92015b52ab8d79480bd54013fdf31e667c
mcostaca <matheuscostaca156@gmail.com>
2023-02-26 21:55:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid-container'>
+      <div className='coluna'>
+        <img src="https://cdn.dribbble.com/users/423261/screenshots/6604674/_x2face2a.gif"></img>
+        <p>Some stuff here</p>
+        <p>Another stuff here, like social media etc</p>
+      </div>
+      <div className='scroll'>
+       
+        <p>Lorem ipsum dolor sit amet. Ut enim rerum aut quasi aliquid est modi voluptatem qui culpa doloribus eum eveniet provident ut autem natus. Et ipsam possimus id velit rerum et quia voluptatibus quo placeat earum sed veritatis quae qui quae voluptatem ad facere voluptas. In voluptate omnis in aliquam necessitatibus ea fugiat internos? Et perspiciatis quasi et commodi vero sit nulla adipisci. Vel ullam ipsa est atque sunt et quas corporis qui quia voluptas et earum sunt. Et fugiat perferendis qui aperiam quis est debitis architecto sed magni possimus et facilis inventore. Ea voluptatem asperiores aut tempore accusamus vel fugiat ratione et expedita commodi sed quibusdam dolorem id provident consequuntur qui eaque beatae. Qui internos corrupti qui eveniet aspernatur qui doloremque iusto ut obcaecati impedit. Ad eaque nobis et unde temporibus sit quia provident. At vitae excepturi qui dolorum eaque in natus optio qui omnis dolor. Sit architecto reprehenderit 33 ullam voluptates qui reprehenderit error et adipisci quos non earum fugit.
+
+Sit dolorem galisum est ipsam tempore ut maxime dolorem sit voluptatum doloremque rem reiciendis porro qui enim velit. Qui dolorum porro ut fugiat omnis nam dolorum laboriosam aut consectetur aliquam aut architecto dolore. Eum libero deserunt sed excepturi suscipit ea labore nemo. Cum magnam optio sed modi voluptatum 33 voluptate quia nam assumenda perferendis. Sed enim internos rem dolorem quod in sint dolor. Et amet alias et voluptates neque vel optio quod sit maxime enim rem obcaecati molestiae qui quasi quia. Ex repellat fugiat ut provident nihil in doloremque possimus ut esse suscipit et aliquid consequuntur. Sed quia expedita qui ipsam voluptas ut sint error ex quia architecto sit consequatur unde nam laborum recusandae ab esse officia.
+
+Nam Quis fugiat hic architecto repellendus est nemo beatae. Et necessitatibus illum ea debitis rerum sit exercitationem cumque. Quo molestiae praesentium est veritatis voluptatem qui nisi deserunt qui neque obcaecati. Ea eligendi tempore et accusamus galisum eos autem officiis in eligendi quisquam. Et voluptatem illum ea accusantium ipsam est quibusdam velit eos fugit itaque. Ut consequatur atque sed dolore molestiae nam sunt facilis et error quisquam est nulla corrupti ut ullam consequatur non dolorem impedit. Vel quis nihil eum nisi consequatur aut voluptatibus blanditiis est nulla facere. Ea optio earum in quia dolore hic assumenda voluptas vel tempore assumenda. 33 alias fugit rem quia fugit et labore vero. Et beatae magnam hic quod autem et fuga repudiandae 33 corrupti harum sit architecto autem. Eos veritatis ratione est eligendi officia qui eius Quis et molestiae minima.</p>
+      </div>
     </div>
   );
 }
